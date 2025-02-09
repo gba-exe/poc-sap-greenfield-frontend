b7de43c30ce185e500ae29ad543e499b99e3618c
gba-exe <125586278+gba-exe@users.noreply.github.com>
2025-02-09 19:32:10 -0300

    feat(Role Name, Transaction):   Initial Form validation, navigation between screens.

---
+++ src/app/page.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+export default function Home() {
+  const router = useRouter();
+
+  const handleSubmit = (e: React.MouseEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!e.currentTarget.checkValidity()) {
+      return;
+    }
+
+    router.push("/transactions");
+  };
+
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center min-h-screen py-2">
+        <h1 className="text-6xl font-bold pb-10">POC SAP Greenfield</h1>
+        <form
+          className="flex flex-col items-start justify-center gap-4 w-fit"
+          onSubmit={handleSubmit}
+        >
+          <label className="text-2xl font-bold">Enter Role Name: </label>
+          <input
+            type="text"
+            className="border border-black rounded-md p-2"
+            placeholder="Transaction"
+            required
+          />
+          <button
+            type="submit"
+            className="bg-black text-white font-bold rounded-md px-4 py-2 w-full"
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+    </>
+  );
+}
---
+++ src/app/transactions/page.tsx
@@ -0,0 +1,119 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+export default function Transactions() {
+  const [inputs, setInputs] = useState<number[]>([]);
+
+  const router = useRouter();
+
+  const saveAndReturn = () => {
+    router.push("/");
+  };
+
+  const createBusinessRole = () => {
+    router.push("/business-role");
+  };
+
+  const handleSubmit = (
+    e: React.FormEvent<HTMLFormElement>,
+    func: () => void,
+  ) => {
+    e.preventDefault();
+
+    if (!e.currentTarget.checkValidity()) {
+      return;
+    }
+
+    func();
+  };
+
+  const addInput = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    setInputs((prev) => [...prev, Math.random()]);
+  };
+
+  const removeInput = (id: number) => {
+    setInputs((prev) => prev.filter((input) => input !== id));
+  };
+
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center min-h-screen py-2">
+        <h1 className="text-6xl font-bold pb-10">POC SAP Greenfield</h1>
+        <form
+          className="flex flex-col items-start justify-center gap-4 w-fit"
+          onSubmit={(e) => {
+            const nativeEvent = e.nativeEvent as SubmitEvent;
+            const submitter = nativeEvent.submitter as HTMLButtonElement;
+            const action = submitter?.name;
+
+            if (action === "save-and-return") {
+              handleSubmit(e, saveAndReturn);
+            } else if (action === "create-business-role") {
+              handleSubmit(e, createBusinessRole);
+            }
+          }}
+        >
+          <label className="text-2xl font-bold">Enter Transaction: </label>
+          <div className="flex flex-row items-center justify-center gap-2">
+            <input
+              type="text"
+              className="border border-black rounded-md p-2"
+              placeholder="Transaction"
+              required
+            />
+            <button
+              className="bg-black text-white font-bold rounded-md px-4 py-2"
+              onClick={addInput}
+            >
+              +
+            </button>
+          </div>
+          {inputs.map((id) => (
+            <div
+              key={id}
+              className="flex flex-row items-start justify-center gap-2"
+            >
+              <input
+                type="text"
+                className="border border-black rounded-md p-2"
+                placeholder="Transaction"
+                required
+              />
+              <button
+                className="bg-black text-white font-bold rounded-md px-4 py-2"
+                onClick={addInput}
+              >
+                +
+              </button>
+              <button
+                className="bg-black text-white font-bold rounded-md px-4 py-2"
+                onClick={() => removeInput(id)}
+              >
+                -
+              </button>
+            </div>
+          ))}
+          <div className="flex flex-row items-center justify-center gap-2 w-full">
+            <button
+              type="submit"
+              name="save-and-return"
+              className="box-border bg-white border-black border-2 text-black font-bold rounded-md p-2 min-w-fit w-full"
+            >
+              Save and Return
+            </button>
+            <button
+              type="submit"
+              name="create-business-role"
+              className="box-border bg-black border-black border-2 text-white font-bold rounded-md p-2 min-w-fit w-full"
+            >
+              Create Business Role
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+}
