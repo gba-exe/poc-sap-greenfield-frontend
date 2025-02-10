23976605d52b3e1a7bec6bd47bcbcb78eb98f7f6
gba-exe <125586278+gba-exe@users.noreply.github.com>
2025-02-09 22:01:31 -0300

    feat(favicon): added favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "POC Sap Greenfield",
   description: "POC Sap Greenfield",
+  icons: {
+    icon: "/favicon.png", 
+  },
 };
 
 export default function RootLayout({
