0322075689e5890a59b4f977a5536ce2234f5b60
gba-exe <125586278+gba-exe@users.noreply.github.com>
2025-02-09 22:49:31 -0300

    feat(validation): adde full validation for rolename

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,66 @@
 "use client";
 
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function Home() {
   const router = useRouter();
 
-  const handleSubmit = (e: React.MouseEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  const [roleName, setRoleName] = useState("");
 
-    if (!e.currentTarget.checkValidity()) {
-      return;
+  const validate = (roleName: string) => {
+    roleName = roleName.toUpperCase();
+
+    if (roleName.length > 30) {
+      return "Role name must less than 3 characters long";
     }
 
-    router.push("/transactions");
+    if (roleName.slice(0, 1) != "Z") {
+      return "Role name must start with Z";
+    }
+
+    if (!roleName.slice(1, 2).match("^[ST]$")) {
+      return "Role name must have S or T after Z";
+    }
+
+    if (!roleName.slice(2, 3).match("^[BCDFHIJKLMPQSVWXY]$")) {
+      return "Role name must have B, C, D, F, H, J, K, L, M, P, Q, S, V, W, X, Y after ZS or ZT";
+    }
+
+    if (
+      !roleName
+        .slice(3, 5)
+        .match("^(BC|CO|PP|FI|HR|IM|PS|MM|QM|SD|PL|SM|CM|TM|PM|CC)$")
+    ) {
+      return "Role name must have BC, CO, PP, FI, HR, IM, PS, MM, QM, SD, PL, SM, CM, TM, PM, CC after ZS or ZT";
+    }
+
+    if (!roleName.slice(5, 7).match("^(DI|MA|CR|AT|AF|AU)$")) {
+      return "Role name must have DI, MA, CR, AT, AF, AU after ZS or ZT";
+    }
+
+    if (roleName.slice(7, 8) != ":") {
+      return "Role name must have a colon after ZS or ZT";
+    }
+
+    if (roleName.slice(8).length < 2) {
+      return "Role name must have at least 2 characters after colon";
+    }
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const input = e.currentTarget.querySelector("input"); // Get the input field
+    if (!input) return;
+
+    const error = validate(roleName);
+    input.setCustomValidity(error || "");
+    input.reportValidity();
+
+    input.setCustomValidity("");
+    if (!error) {
+      router.push("/transactions");
+    }
   };
 
   return (
@@ -21,14 +69,15 @@ export default function Home() {
         <h1 className="text-6xl font-bold pb-10">POC SAP Greenfield</h1>
         <form
           className="flex flex-col items-start justify-center gap-4 w-fit"
-          onSubmit={handleSubmit}
+          onSubmit={(e) => handleSubmit(e)}
         >
           <label className="text-2xl font-bold">Enter Role Name: </label>
           <input
             type="text"
             className="border border-black rounded-md p-2"
             placeholder="Transaction"
-            pattern="(?i)(ZS:.*)"
+            value={roleName}
+            onChange={(e) => setRoleName(e.target.value)}
             required
           />
           <button
